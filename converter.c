33eca48056c7639e8a83d128bbdcae2bbf8aed90
Huseini Yusif <yusifhuseini@gmail.com>
2023-04-23 15:50:52 -0700

    yusifhuseini contribution

---
+++ converter.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * convert - converts number and base into string
+ * @num: num input
+ * @base: base input
+ * @lowercase: lowercase hexa values
+ * Return: ptr value
+ */
+char *convert(unsigned long int num, int base, int lowercase)
+{
+	static char *rep;
+	static char buffer[50];
+	char *ptr;
+
+	rep = (lowercase)
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
+
+	return (ptr);
+}
